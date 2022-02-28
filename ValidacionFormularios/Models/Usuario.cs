32af242c92712d9d4632ec0c8fae9487bf2a7380
MariMar9 <garciacarrionmar@gmail.com>
2022-02-28 17:00:21 +0100

    Agregar archivos de proyecto.

---
+++ ValidacionFormularios/Models/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ValidacionFormularios.Models
+{
+    public class Usuario
+    {
+        [Required]
+        [StringLength(100, ErrorMessage="El nombre no puede contener más de 100 caracteres", MinimumLength = 2)]
+        public String Nombre { get; set; }
+
+        public String Apellidos { get; set; }
+
+        [Required]
+        public int Edad { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public String Email { get; set; }
+
+        [Required]
+        public String Contrasenia { get; set; }
+
+        [Required]
+        public String ConfirmarContrasenia { get; set; }
+    }
+}
