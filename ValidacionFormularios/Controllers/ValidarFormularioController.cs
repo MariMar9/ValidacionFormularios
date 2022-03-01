8e35af16bbee7138bc19517cd7a4f305dbeeb13a
MariMar9 <garciacarrionmar@gmail.com>
2022-03-01 23:30:23 +0100

    commit

---
+++ ValidacionFormularios/Controllers/ValidarFormularioController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ValidacionFormularios.Models;
+
+namespace ValidacionFormularios.Controllers
+{
+    public class ValidarFormularioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Usuario usuario)
+        {
+            if (usuario.IsValid(usuario))
+            {
+                ViewBag.Mensaje = "Usuario correcto.";
+                return View();
+            }
+            else
+            {
+                ViewBag.Mensaje = "No tiene edad suficiente para acceder.";
+                return View();
+            }
+        }
+    }
+}
--- ValidacionFormularios/Models/Usuario.cs
+++ ValidacionFormularios/Models/Usuario.cs
@@ -6,25 +6,46 @@ using System.Threading.Tasks;
 
 namespace ValidacionFormularios.Models
 {
-    public class Usuario
+    public class Usuario: ValidationAttribute
     {
-        [Required]
-        [StringLength(100, ErrorMessage="El nombre no puede contener más de 100 caracteres", MinimumLength = 2)]
-        public String Nombre { get; set; }
+        #region ATRIBUTOS
+            [Required(ErrorMessage ="Este campo es requerido.")]
+            [MaxLength(100, ErrorMessage = "El nombre no puede contener más de 100 caracteres.")]
+            [MinLength(2, ErrorMessage = "El nombre no puede contener menos de 2 caracteres.")]
+            public String Nombre { get; set; }
 
-        public String Apellidos { get; set; }
+            public String Apellidos { get; set; }
 
-        [Required]
-        public int Edad { get; set; }
+            [Required(ErrorMessage = "Este campo es requerido.")]
+            [Range(10, 100,ErrorMessage ="La edad debe estar comprendida entre los 10 y los 100 años.")]
+            public int Edad { get; set; }
 
-        [Required]
-        [EmailAddress]
-        public String Email { get; set; }
+            [Required(ErrorMessage = "Este campo es requerido.")]
+            [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage ="Formato incorrecto.")]
+            public String Email { get; set; }
 
-        [Required]
-        public String Contrasenia { get; set; }
+            [Required(ErrorMessage = "Este campo es requerido.")]
+            [RegularExpression(@"^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$", ErrorMessage = "La contraseña debe tener entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula.")]
+            public String Contrasenia { get; set; }
 
-        [Required]
-        public String ConfirmarContrasenia { get; set; }
+            [Required(ErrorMessage = "Este campo es requerido.")]
+            [Compare("Contrasenia", ErrorMessage ="Las contraseñas no coinciden.")]
+            public String ConfirmarContrasenia { get; set; }
+        #endregion
+
+        #region MÉTODOS
+            public override bool IsValid(object usuario)
+            {
+                Usuario objeto = (Usuario)usuario;
+                if (objeto.Edad >= 18)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        #endregion
     }
 }
